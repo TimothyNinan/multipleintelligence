58433e0031ee1ccfd808f2b542a8bdac3e919399
TimothyNinan <1.tninan@gmail.com>
2020-09-29 19:39:40 -0400

    admin?

--- mi/admin.py
+++ mi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Class, Questions, Results
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Class)
+admin.site.register(Questions)
+admin.site.register(Results)
